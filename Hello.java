618e16b938f5d97e8630f08bc7b78fe8c4702349
ben <928409508@qq.com>
2019-11-15 12:18:26 +0800

    ben

--- Hello.java
+++ Hello.java
@@ -1,10 +1,9 @@
 public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello World");
-<<<<<<< HEAD
 		System.out.println("ben");
-=======
+
 		System.out.println("Hello hht");
->>>>>>> f786b57f3a5ac5654d415973e6053188838b4538
+
 	}
 }
